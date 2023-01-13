c06118517fe27f81f8156d4d35f40934f14cbfbf
Eunhyung Choi <milkiss0414@naver.com>
2023-01-13 22:23:02 +0900

    fix: favicon 오류

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
         <meta name="description" content="쉽고 똑똑한 나만의 쇼핑 도우미, 온사이즈" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
